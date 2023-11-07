3cccac8604b14d86ea5c01d3201ae017c0922e99
Antonin Carlin <antonin.carlin@icloud.com>
2023-11-07 17:40:56 +0100

    fresh install script

---
+++ fresh-install.sh
@@ -0,0 +1,7 @@
+# install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install brew packages git zsh tmux, vscode, chrome, iterm2, docker, slack, telegram
+brew install git zsh tmux
+brew install --cask visual-studio-code google-chrome iterm2 docker slack telegram
+
